a667b2b6de157714d6c8de8de8ea1bdd7746f281
Si Thu San <sithusanss@gmail.com>
2025-12-20 13:51:40 +0700

    build: setup gemini api

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from llm-shell!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("API Key required")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is LLM Shell is so cool? Use one paragraph maximum.",
+    )
+
+    print(response.text)
 
 
 if __name__ == "__main__":
