ed9bebfd3f42cba9cc2ecf6f47510839b6d5dbc9
Si Thu San <sithusanss@gmail.com>
2025-12-20 14:39:54 +0700

    feat: user prompt

--- main.py
+++ main.py
@@ -7,13 +7,26 @@ from google.genai import types
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = getEnv("GEMINI_API_KEY")
 
-    if not api_key:
-        raise RuntimeError("GEMINI_API_KEY required")
+    prompt = getUserPrompt()
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt.user_prompt)])]
 
-    messages = [types.Content(role="user", parts=[types.Part(text=getUserPrompt())])]
+    response = getResponseFromGenAI(api_key=api_key, messages=messages)
 
+    displyResponse(prompt=prompt, response=response)
+
+
+def getEnv(key):
+    val = os.environ.get(key)
+
+    if not val:
+        raise RuntimeError(f"{key} required")
+
+    return val
+
+
+def getResponseFromGenAI(api_key, messages):
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages
@@ -22,16 +35,24 @@ def main():
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response malformed")
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+    return response
 
 
 def getUserPrompt():
     parser = argparse.ArgumentParser(description="LLM Shell")
     parser.add_argument("user_prompt", type=str, help="User Prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 
-    return parser.parse_args().user_prompt
+    return parser.parse_args()
+
+
+def displyResponse(prompt, response):
+    if prompt.verbose:
+        print(f"User prompt: {prompt.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
