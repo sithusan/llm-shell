a65b55cf16bd1591e88e7904c0b3bc6ee79108e8
Si Thu San <sithusanss@gmail.com>
2025-12-21 14:21:59 +0700

    fix: use the absolute path for the creating parent directories

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,9 +5,7 @@ from functions.helpers import is_valid_target
 
 def write_file(working_directory, file_path, content):
     try:
-        target_file = get_target(
-            working_directory=working_directory, target=file_path
-        )
+        target_file = get_target(working_directory=working_directory, target=file_path)
 
         # Without this restriction, the LLM might run amok anywhere on the machine, reading sensitive files or overwriting important data
         if not is_valid_target(working_directory=working_directory, target=file_path):
@@ -16,7 +14,7 @@ def write_file(working_directory, file_path, content):
         if os.path.isdir(target_file):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
 
-        os.makedirs(file_path, exist_ok=True)
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
 
         file = open(target_file, "w")
         wrote = file.write(content)
