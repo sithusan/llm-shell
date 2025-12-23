e34ae8f16b8409668f423de4c1eaf0de853a05c1
Si Thu San <sithusanss@gmail.com>
2025-12-23 22:33:01 +0700

    feat: function calling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,12 @@
 import os
 from functions.helpers import get_target
 from functions.helpers import is_valid_target
+from google.genai import types
 
 
 def get_files_info(working_directory, directory="."):
     try:
-        target_dir = get_target(
-            working_directory=working_directory, target=directory
-        )
+        target_dir = get_target(working_directory=working_directory, target=directory)
 
         # Without this restriction, the LLM might run amok anywhere on the machine, reading sensitive files or overwriting important data
         if not is_valid_target(working_directory=working_directory, target=target_dir):
@@ -27,3 +26,18 @@ def get_files_info(working_directory, directory="."):
         return "\n".join(result)
     except Exception as e:
         return f"Error: {e}"
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import prompts
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.call_function import available_functions
 
 
 def main():
@@ -32,7 +33,9 @@ def getResponseFromGenAI(api_key, messages):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=prompts.system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=prompts.system_prompt
+        ),
     )
 
     if not response.usage_metadata:
@@ -55,7 +58,12 @@ def displyResponse(prompt, response):
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
 
-    print(f"Response: {response.text}")
+    # Not guarantee to be a list
+    if response.function_calls is not None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
